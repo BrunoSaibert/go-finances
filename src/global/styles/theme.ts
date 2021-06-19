dcf2284ee3b7e48d05ee904236391496a9ae0ca6
Bruno Saibert <brunosaibert@gmail.com>
2021-06-19 14:35:55 -0300

    feat: adicionar fonte presonalizada

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -16,4 +16,10 @@ export default {
     text: "#969cb2",
     background: "#f0f2f5",
   },
+
+  fonts: {
+    regular: "Poppins_400Regular",
+    medium: "Poppins_500Medium",
+    bold: "Poppins_700Bold",
+  },
 };
